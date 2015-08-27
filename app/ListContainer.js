d34ec1bd82b412585f6b5537593647b07053ab88
Evan Rosse <rosse@brdf01w21.bklyndigi.local>
2015-08-27 13:15:51 -0400

    perseisted data to firebase

--- app/ListContainer.js
+++ app/ListContainer.js
@@ -2,24 +2,37 @@ console.log("list container loaded")
 var React = require('react');
 var AddItem = require('./AddItem.js');
 var List = require('./List.js');
+var Firebase = require("firebase");
 
 var ListContainer = React.createClass({
+	componentDidMount: function(){
+		this.firebaseRef = new Firebase('https://scorching-torch-5024.firebaseio.com');
+		this.firebaseRef.on("child_added", function(snapshot){
+			this.setState({
+				list: this.state.list.concat([{key: snapshot.key(), val: snapshot.val()}])
+			})
+		}.bind(this));
+		this.firebaseRef.on("child_removed", function(snapshot){
+			var key = snapshot.key();
+			var newList = this.state.list.filter(function(item){
+				return item.key !== key;
+			});
+			this.setState({
+				list: newList
+			});
+		}.bind(this));
+	},
 	getInitialState: function(){
 		return { 
 			list: []
 		}
 	},
 	handleAddItem: function(newItem){
-		this.setState({
-			list: this.state.list.concat([newItem])
-		});
+		this.firebaseRef.push(newItem);
 	},
 	handleRemoveItem: function(index){
-		var newList = this.state.list;
-		newList.splice(index, 1);
-		this.setState({
-			list: newList
-		})
+		var item = this.state.list[index];
+		this.firebaseRef.child(item.key).remove();
 	},
 	render: function(){
 		return (
@@ -27,7 +40,7 @@ var ListContainer = React.createClass({
 		        <div className="col-sm-12">
 		          <h3 className="text-center"> Todo List </h3>
 		          <AddItem add={this.handleAddItem} />
-		          <List items={this.state.list} remove={this.handleRemoveItem} />
+		          <List items={this.state.list.map(function(item){return item.val})} remove={this.handleRemoveItem} />
 		        </div>
 		    </div>
 		)
